09be8ebeb1704e6658d722884b74ddf67e380a46
absyah <ardian.bahtiarsyah@gmail.com>
2015-09-12 21:35:57 +0700

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name inkodeApp
+ * @description
+ * # inkodeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('inkodeApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
