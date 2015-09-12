a262cb7cea2731cd40e0f6084fad0a55e74e4308
absyah <ardian.bahtiarsyah@gmail.com>
2015-09-12 21:42:56 +0700

    added env setting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,21 @@
  *
  * Main module of the application.
  */
+
+var currentEnv = "dev";
+var settings = {
+  endpoints: {
+    "dev": "",
+    "staging": "",
+    "production": ""
+  },
+
+  endpoint: function() {
+    return settings.endpoints[currentEnv];
+  }
+};
+
+
 angular
   .module('inkodeApp', [
     'ngAnimate',
