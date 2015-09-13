4ea4734320143eae811b8f4077b666407c37d131
absyah <ardian.bahtiarsyah@gmail.com>
2015-09-13 20:52:34 +0700

    fixed mailchimp subscription process

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,8 @@
  * Controller of the inkodeApp
  */
 angular.module('inkodeApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', ['$log', '$resource', '$scope', '$rootScope',
+              function ($log, $resource, $scope, $rootScope) {
     
     function designInit() {
       $scope.page = {
@@ -42,23 +43,72 @@ angular.module('inkodeApp')
               "</html>"
     }
 
-    $scope.addSubscription = function(mailchimp) {
-      var mailchimpParams = {
-        EMAIL: mailchimp.EMAIL
-      }
-
-      $http
-        .post('https://herokuapp.us11.list-manage.com/subscribe/post?u=f19bab9ca00ea8b1bf55b2209&id=b64473e5af', mailchimpParams)
-        .then(function(response) {
-          mailchimpInit();
-          $scope.mailchimp.success = true;
-        }, function(error) {
-          $scope.mailchimp.error = true;
-        })
-    }
-
     $scope.resetDesign = function() {
       designInit();
     }
 
-  });
+    $scope.addSubscription = function (mailchimp) {
+      var actions,
+          MailChimpSubscription,
+          params = {},
+          url;
+
+      // Create a resource for interacting with the MailChimp API
+      url = '//' + mailchimp.username + '.' + mailchimp.dc +
+            '.list-manage.com/subscribe/post-json';
+
+      var fields = Object.keys(mailchimp);
+
+      for(var i = 0; i < fields.length; i++) {
+        params[fields[i]] = mailchimp[fields[i]];
+      }
+
+      params.c = 'JSON_CALLBACK';
+
+      actions = {
+        'save': {
+          method: 'jsonp'
+        }
+      };
+      MailChimpSubscription = $resource(url, params, actions);
+
+      // Send subscriber data to MailChimp
+      MailChimpSubscription.save(
+        // Successfully sent data to MailChimp.
+        function (response) {
+          // Define message containers.
+          mailchimp.errorMessage = '';
+          mailchimp.successMessage = '';
+
+          // Store the result from MailChimp
+          mailchimp.result = response.result;
+
+          // Mailchimp returned an error.
+          if (response.result === 'error') {
+            if (response.msg) {
+              // Remove error numbers, if any.
+              var errorMessageParts = response.msg.split(' - ');
+              if (errorMessageParts.length > 1)
+                errorMessageParts.shift(); // Remove the error number
+              mailchimp.errorMessage = errorMessageParts.join(' ');
+            } else {
+              mailchimp.errorMessage = 'Sorry! An unknown error occured.';
+            }
+          }
+          // MailChimp returns a success.
+          else if (response.result === 'success') {
+            mailchimp.successMessage = response.msg;
+          }
+
+          //Broadcast the result for global msgs
+          $rootScope.$broadcast('mailchimp-response', response.result, response.msg);
+        },
+
+        // Error sending data to MailChimp
+        function (error) {
+          $log.error('MailChimp Error: %o', error);
+        }
+      );
+    };
+
+  }]);
